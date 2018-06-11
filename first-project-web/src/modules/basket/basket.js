957b2bad0c58c8bf34d29375619ffdeedff6374c
DELIMEUX FREDERIC <frederic.delimeux@decathlon.com>
2018-06-11 17:36:04 +0200

    first router

---
+++ first-project-web/src/modules/basket/basket.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Basket extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is the basket !</h2>
+      </div>
+    )
+  }}
+
+export default Basket;
---
+++ first-project-web/src/modules/router/router.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink
+} from 'react-router-dom';
+
+import Home from './../home/home.js'
+import Basket from './../basket/basket.js'
+import Products from './../products/products.js'
+import ProductView from './../productview/productview.js'
+
+
+
+class LocalRouter extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/products" component={Products}/>
+          <Route path="/product/:productId" component={ProductView}/>
+          <Route exact path="/basket" component={Basket}/>
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default LocalRouter
