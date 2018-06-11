429ed00a4d6692f69efd7ff34cb71749a3840b1d
JULIEN LI <31632821+JulienL-Deca@users.noreply.github.com>
2018-06-11 19:16:08 +0200

    Categories: using componentDidUpdate to update component render when needed

--- first-project-web/src/modules/categories/Categories.js
+++ first-project-web/src/modules/categories/Categories.js
@@ -10,21 +10,42 @@ class Categories extends Component {
   }
 
   componentDidMount(){
-    this.getCategories();
-  }
-
-  getCategories() {
     axios.get('https://decath-product-api.herokuapp.com/categories')
     .then((response) => this.setState({listCategories: response.data}))
-    //                  this.setState({report: response.data})
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if (prevProps.listCategories !== this.props.listCategories) {
+      this.print1line()
+    }
+  }
+
+  print1line(){
+    return this.state.listCategories.map((oneCategory, index) => {
+      return(
+        <tr>
+          <td key={index}>{oneCategory.label}</td>
+          <td>{oneCategory.id}</td>
+        </tr>
+      )
+    })
   }
 
   render () {
     return (
     <div>
-        <h1> Hello Julien</h1>
-        {/* {this.state.listCategories[0]} */}
-        {console.log(this.state.listCategories[0])}
+      <h1> Hello Julien</h1>
+      <table>
+        <thead>
+          <tr>
+            <td>Category name</td>
+            <td>Category UUID</td>
+          </tr>
+        </thead>
+        <tbody>
+          {this.print1line()}
+        </tbody>
+      </table>
     </div>
     )
   }
