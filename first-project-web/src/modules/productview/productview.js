f0b68dbe9cbda70e7eab19dbcbe1c1a84f23a6f2
DELIMEUX FREDERIC <frederic.delimeux@decathlon.com>
2018-06-12 14:42:14 +0200

    update navbar

--- first-project-web/src/modules/productview/productview.js
+++ first-project-web/src/modules/productview/productview.js
@@ -1,33 +1,48 @@
 import React, {Component} from 'react'
 import axios from 'axios';
 
+class ProductCard extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.product.title}
+        {this.props.product.description}
+        {this.props.product.image_path}
+        {this.props.product.min_price}
+        {this.props.product.rating}
+      </div>
+    )
+  }
+}
+
 class ProductView extends Component {
   constructor(props){
     super(props);
     this.state = {
-      listCategories: []
+      productView: []
     }
   }
 
   componentDidMount(){
     axios.get(`https://decath-product-api.herokuapp.com/products/${this.props.match.params.productId}`)
-    .then((response) => this.setState({listCategories: response.data}))
+    .then((response) => this.setState({productView: response.data}))
   }
 
   componentDidUpdate(prevProps, prevState){
-    if (prevProps.listCategories !== this.props.listCategories) {
+    if (prevProps.productView !== this.props.productView) {
       this.print1line()
     }
   }
-    render() {
-      return (
-        <div>
-            <h2>This is the product view</h2>
 
-          {this.props.match.params.productId}
-        </div>
-      )
-    }
+
+  render () {
+    console.log(this.state.productView);
+    return (
+    <div id="page_container" className="col-10 offset-1">
+      <ProductCard product={this.state.productView} />
+    </div>
+    )
+  }
 
 }
 
