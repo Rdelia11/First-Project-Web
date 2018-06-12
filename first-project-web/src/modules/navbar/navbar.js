698e48d4a563fddc157ab653e23f8fe43f53e2cd
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-06-12 18:11:12 +0200

    product page last plus google connect

--- first-project-web/src/modules/navbar/navbar.js
+++ first-project-web/src/modules/navbar/navbar.js
@@ -11,7 +11,8 @@ function Navbar(props){
       </div>
       <div id="container">
         <div id="connectButton">
-          <button type="button" className="btn btn-outline-light">Login</button>
+          <div className="g-signin2" data-onsuccess="googleConnectCallback" data-theme="dark">
+          </div>
         </div>
         <div id="logged">
           <span id="connected">Hello firstName</span>
@@ -23,5 +24,9 @@ function Navbar(props){
     </div>
   );
 }
+
+
+
+
 
 export default Navbar;
