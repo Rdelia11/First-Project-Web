2ff2701dfa43191ff4154704c71b2c2fe06f57ce
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-06-12 14:44:34 +0200

    last nico

--- first-project-web/src/modules/productview/productview.js
+++ first-project-web/src/modules/productview/productview.js
@@ -1,15 +1,66 @@
 import React, {Component} from 'react'
+import axios from 'axios';
+
+class ProductCard extends Component {
+  render() {
+    const url="https://www.decathlon.fr/media/"+this.props.product.image_path;
+    return (
+      <div>
+      <h1>Product detail</h1>
+      <div id="container_ProductCard">
+
+        <div id="child_image">
+          <img src={url}/>
+        </div>
+
+        <div id="child_blocInfo">
+
+          <h4>{this.props.product.title}</h4>
+          <p>{this.props.product.description}</p>
+          <hr></hr>
+
+          <span id="price">{this.props.product.min_price} €</span>
+
+          <p>Customer rating : {this.props.product.rating} /5</p>
+          <a href="#" className="btn btn-primary">Add to cart &gt;</a>
+        </div>
+
+
+      </div>
+    </div>
+    )
+  }
+}
 
 class ProductView extends Component {
-    render() {
-      return (
-        <div>
-            <h2>This is the product view</h2>
-
-          {this.props.match.params.productId}
-        </div>
-      )
+  constructor(props){
+    super(props);
+    this.state = {
+      productView: []
     }
+  }
+
+  componentDidMount(){
+    axios.get(`https://decath-product-api.herokuapp.com/products/${this.props.match.params.productId}`)
+    .then((response) => this.setState({productView: response.data}))
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if (prevProps.productView !== this.props.productView) {
+      this.print1line()
+    }
+  }
+
+
+  render () {
+    console.log(this.state.productView);
+    return (
+    <div id="page_container" className="col-10 offset-1">
+
+      <ProductCard product={this.state.productView} />
+    </div>
+    )
+  }
 
 }
 
