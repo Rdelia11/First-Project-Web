5666a6c9fb77d78a026bd9dd01b28954cc76fd31
JULIEN LI <31632821+JulienL-Deca@users.noreply.github.com>
2018-06-13 12:37:19 +0200

    la pastille de navbar.js affiche le nombre d'articles venant du state

--- first-project-web/src/modules/navbar/navbar.js
+++ first-project-web/src/modules/navbar/navbar.js
@@ -1,42 +1,42 @@
 import React, { Component } from 'react';
-// import { GoogleLogin } from 'react-google-login';
+import {mapStateToProps} from './../../store/basket/selector.js'
+import {connect} from 'react-redux';
 
-// const responseGoogle = (response) => {
-//   console.log(response);
-// }
 
-function Navbar(props){
-  return(
-    <div id="navbar" className="fixed-top">
-      <div id="home">
-        <a href="/"><i className="fas fa-home"></i></a>
-      </div>
-      <div id="logo">
-        <img src={"https://nameless-cliffs-89719.herokuapp.com/images/logo.png"} width="100" alt="logo decathlon"/>
-      </div>
-      <div id="container">
-        <div id="connectButton">
-          {/* <GoogleLogin clientId="975507228152-s6o2o4cnih74js8prhaoru6bhnj152lk.apps.googleusercontent.com" buttonText="Login" onSuccess={responseGoogle} onFailure={responseGoogle}/> */}
-          <div className="g-signin2" data-onsuccess="googleConnectCallback" data-theme="dark">
+class Navbar extends Component{
+
+  howmanyArticleInBasket(){
+    let nbArticleInBasket = 0;
+    this.props.productsInBasket.forEach(oneProduct => nbArticleInBasket += oneProduct.quantity);
+    return nbArticleInBasket;
+  }
+
+  render (){
+    return(
+      <div id="navbar" className="fixed-top">
+        <div id="home">
+          <a href="/"><i className="fas fa-home"></i></a>
+        </div>
+        <div id="logo">
+          <img src={"https://nameless-cliffs-89719.herokuapp.com/images/logo.png"} width="100" alt="logo decathlon"/>
+        </div>
+        <div id="container">
+          <div id="connectButton">
+            {/* <GoogleLogin clientId="975507228152-s6o2o4cnih74js8prhaoru6bhnj152lk.apps.googleusercontent.com" buttonText="Login" onSuccess={responseGoogle} onFailure={responseGoogle}/> */}
+            <div className="g-signin2" data-onsuccess="googleConnectCallback" data-theme="dark">
+            </div>
+          </div>
+          <div id="logged">
+            <span id="connected">Hello firstName</span>
+          </div>
+          <div id="cart">
+            <a href="/basket"><i className="fas fa-shopping-cart"></i></a>
+            <div className="bottom-right">&nbsp;{this.howmanyArticleInBasket()}&nbsp;</div>
           </div>
         </div>
-        <div id="logged">
-          <span id="connected">Hello firstName</span>
-        </div>
-        <div id="cart">
-          <a href="/basket"><i className="fas fa-shopping-cart"></i></a>
-          <div className="bottom-right">&nbsp;1&nbsp;</div>
-        </div>
       </div>
-    </div>
-
-  );
+    );
+  }
 }
 
-
-
-
-
-
-
-export default Navbar;
+export default connect(mapStateToProps, null)(Navbar);
