89715b4c23b3421556ba8f6e7641360c117f6018
NICOLAS PROUVOST <nicolas.prouvost@decathlon.com>
2018-06-13 14:28:54 +0200

    Products page +  and a bit more CSS

--- first-project-web/src/modules/products/products.js
+++ first-project-web/src/modules/products/products.js
@@ -28,14 +28,15 @@ class Products extends Component {
       return(
         <div id="page_container" className="col-8 offset-2">
         <div key={index}>
-          <div className="card-body">
-          </div>
+          <div className="card">
+
           <div className="card-section">
-            <a href={`/product/${oneProduct.id}`}><h4 className="card-product-name">{oneProduct.title}</h4></a>
+            <h4 className="card-product-name">{oneProduct.title}</h4>
             <a href={`/product/${oneProduct.id}`}><img src={`https://www.decathlon.fr/media/${oneProduct.image_path}`} alt="Product"></img></a>
-            <h5 className="card-product-price">{oneProduct.min_price}€</h5>
+            <div className="price pt-4">{oneProduct.min_price}€</div>
             <p className="card-product-description">{oneProduct.description}</p>
-            <a href="#" className="btn btn-primary">Add to basket</a>
+            <a href={`/product/${oneProduct.id}`} className="btn btn-outline-primary">See product page</a><a href="#" className="btn btn-primary ml-3">Add to basket</a>
+          </div>
           </div>
         </div>
         </div>
@@ -46,27 +47,23 @@ class Products extends Component {
   hundleChange = (e) => {
     let searchResult = [];
     console.log(e.target.value.length);
-    if (e.target.value.length === 0) {
-      return searchResult = this.state.listProducts;
-    } else {
       this.state.listProducts.filter(item => {
-        if (item.title.includes(e.target.value)) {
+        if (item.title.toUpperCase().includes(e.target.value.toUpperCase())) {
           return searchResult.push(item);
         }
       })
-    }
-    //console.log(e.target.value)
     this.setState({FilterView : searchResult})
-
   }
+
   render () {
     return (
       <div id="page_container">
+
+        <h1 className="body">Sports products</h1>
         <form>
-          <input className="SearchBar" type="text" placeholder="Product" onChange={(e)=> this.hundleChange(e)} >
+          <input className="SearchBar" type="text" placeholder="Search a product..." onChange={(e)=> this.hundleChange(e)} >
           </input>
         </form>
-        <h1 className="body">Sports products</h1>
         {this.print1line()}
       </div>
     )
