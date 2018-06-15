7261ed9d34ec42c22fb71ff6cb0e492b8544746d
Julien Li <31632821+JulienL-Deca@users.noreply.github.com>
2018-06-15 15:23:35 +0200

    Update handlers.js

--- src/store/basket/handlers.js
+++ src/store/basket/handlers.js
@@ -4,5 +4,6 @@ export function cartAction(dispatch) {
     addmoreqte: (article, qte) => dispatch({type : "ADD_MORE_QUANTITY", product: article, quantity: qte}),
     delqte: (articleid) => dispatch({type : "DEL_QUANTITY", id: articleid}),
     rmitem: (articleid) => dispatch({type : "REMOVE_ITEM", id: articleid}),
+    rmAll: () => dispatch({type: "REMOVE_ALL"})
   }
 }
